7a46d2d41f87da15a260c85f655e25b2835f70cb
Tabrez <tabrez@mm-in-tabrez.local>
2021-02-14 18:33:22 +0530

    Initial Commit

---
+++ app.py
@@ -0,0 +1,148 @@
+from flask import Flask, jsonify, request
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import Column, Integer, String, Float
+import os
+from flask_marshmallow import Marshmallow
+
+app = Flask(__name__)
+basedir = os.path.abspath(os.path.dirname(__file__))
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'planets.db')
+
+db = SQLAlchemy(app)
+ma = Marshmallow(app)
+
+
+@app.cli.command('db_create')
+def db_create():
+    db.create_all()
+    print('Database Created!')
+
+
+@app.cli.command('db_drop')
+def drop_all():
+    db.drop_all()
+    print('Database dropped!')
+
+
+@app.cli.command('db_seed')
+def db_seed():
+    mercury = Planet(planet_name='Mercury',
+                     planet_type='Class D',
+                     home_star='Sol',
+                     mass=4.867e24,
+                     radius=3760,
+                     distance=67.24e6)
+
+    venus = Planet(planet_name='Venus',
+                   planet_type='Class K',
+                   home_star='Sol',
+                   mass=3.325e23,
+                   radius=1516,
+                   distance=35.98e6)
+
+    earth = Planet(planet_name='Earth',
+                   planet_type='Class M',
+                   home_star='Sol',
+                   mass=5.972e24,
+                   radius=3959,
+                   distance=92.96e6)
+
+    db.session.add(mercury)
+    db.session.add(venus)
+    db.session.add(earth)
+
+    test_user = User(first_name='Tabrez',
+                     last_name='Ahmed',
+                     email='test@test.com',
+                     password='P@ssw0rd')
+
+    db.session.add(test_user)
+    db.session.commit()
+    print('Database seeded!')
+
+
+@app.route('/')
+def hello_world():
+    return jsonify(message='Hello from the Planetary API'), 200
+
+
+@app.route('/parameters')
+def parameters():
+    name = request.args.get('name')
+    age = int(request.args.get('age'))
+    if age < 18:
+        return jsonify(message='Sorry ' + name + '! You are not old enough'), 401
+    else:
+        return jsonify(message='Welcome ' + name + '! You are old enough'), 200
+
+
+@app.route('/url_variables/<string:name>/<int:age>')
+def url_variables(name: str, age: int):
+    if age < 18:
+        return jsonify(message='Sorry ' + name + '! You are not old enough'), 401
+    else:
+        return jsonify(message='Welcome ' + name + '! You are old enough'), 200
+
+
+@app.route('/not_found')
+def not_found():
+    return jsonify(message='That resource was not found'), 200
+
+
+@app.route('/planets', methods=['GET'])
+def get_planets():
+    query_result = Planet.query.all()
+    planets_list = planets_schema.dump(query_result)
+    return jsonify(planets_list)
+
+
+@app.route('/users/register', methods=['POST'])
+def register_user():
+    data = request.get_json();
+    data['id'] = None
+    user = User(**data)
+    db.session.add(user)
+    db.session.commit()
+    return jsonify(valid=True)
+
+
+# database models
+class User(db.Model):
+    __tablename__ = 'users'
+    id = Column(Integer, primary_key=True)
+    first_name = Column(String)
+    last_name = Column(String)
+    email = Column(String, unique=True)
+    password = Column(String)
+
+
+class Planet(db.Model):
+    __tablename__ = 'planets'
+    planet_id = Column(Integer, primary_key=True)
+    planet_name = Column(String)
+    planet_type = Column(String)
+    home_star = Column(String)
+    mass = Column(Float)
+    radius = Column(Float)
+    distance = Column(Float)
+
+
+class UserSchema(ma.Schema):
+    class Meta:
+        fields = ('id', 'first_name', 'last_name', 'email', 'password')
+
+
+user_schema = UserSchema()
+users_schema = UserSchema(many=True)
+
+
+class PlanetSchema(ma.Schema):
+    class Meta:
+        fields = ('id', 'planet_name', 'planet_type', 'home_star', 'mass', 'radius', 'distance')
+
+
+planet_schema = PlanetSchema()
+planets_schema = PlanetSchema(many=True)
+
+if __name__ == '__main__':
+    app.run()
